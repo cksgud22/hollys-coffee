a7873fad65eb17846054ccf353f5a15819f1bfc6
cksgud22 <you@example.com>
2024-07-15 17:44:56 +0900

    first commit

---
+++ js/int4.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".btn1").click(function(){
+        $("body").animate({"background-color" : "red"},1000)
+    })
+}) /* btn1 */
+$(function(){
+    $(".btn2").click(function(){
+        $("body").animate({"background-color" : "yellow"},1000)
+    })
+})/* btn2 */
+$(function(){
+    $(".btn3").click(function(){
+        $("body").animate({"background-color" : "green"},1000)
+    })
+})/* btn3 */
+$(function(){
+    $(".btn4").click(function(){
+        $("body").animate({"background-color" : "blue"},1000)
+    })
+})/* btn4 */
---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+    // 메뉴
+    $(".main > li, .sub_bg").hover(function(){
+        $(".sub").stop().show();
+        $(".sub_bg").stop().slideDown("fast");
+    },function(){
+        $(".sub").stop().hide();
+        $(".sub, .sub_bg").stop().slideUp("fast");
+    }) /* hover */
+
+    $(".move").slick({
+        autoplay:true,
+        dots:true,
+        arrows:false,
+        autoplaySpeed: 2000,
+        adaptiveHeight:true,
+        fade:true,
+    })
+
+
+})/* jqery */
